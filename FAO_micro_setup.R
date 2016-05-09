47e72f449cd25a16150ccc7e961ceaa0eade9e11
mgwalsh <markusgwalsh@gmail.com>
2016-05-09 15:55:16 +0300

    initial train/test set partition

--- FAO_micro_setup.R
+++ FAO_micro_setup.R
@@ -3,18 +3,17 @@
 #' MIR soil data courtesy ICRAF
 #' M. Walsh, May 2016
 
-# install.packages(c("downloader","compositions","colorRamps","RColorBrewer","MASS"), dependencies=T)
+# install.packages(c("downloader"), dependencies=T)
 suppressPackageStartupMessages({
   require(downloader)
-  require(compositions)
-})
+ })
 
 # Data setup --------------------------------------------------------------
 # Create a data folder in your current working directory
 dir.create("FAO_data", showWarnings=F)
 setwd("./FAO_data")
 
-# Download
+# Download 
 download("https://www.dropbox.com/s/hhdoxswpfb9vlz1/FAO_micro_bioavailability.zip?dl=0", "FAO_micro_bioavailability.zip", mode="wb")
 unzip("FAO_micro_bioavailability.zip", overwrite=T)
 cid <- read.table("countries.csv", header=T, sep=",") ## country ID's
@@ -27,6 +26,13 @@ plant <- read.table("plants.csv", header=T, sep=",") ## FAO plant DM yield and m
 wetdat <- merge(soils, plant, by="SSID")
 mirdat <- merge(dat, mir, by="SSID")
 
+# Train/Test set partition ------------------------------------------------
+country <- names(table(mirdat$Country))
+set.seed(1385321)
+train <- sample(country, 0.8*length(country)) ## sample 80% of of countries for calibration
+fao_cal <- mirdat[ mirdat$Country%in%train, ] ## calibration data
+fao_val <- mirdat[!mirdat$Country%in%train, ] ## validation data
+
 # Write data files --------------------------------------------------------
-write.csv(wetdat, "wetdat.csv", row.names=F)
-write.csv(mirdat, "mirdat.csv", row.names=F)
+write.csv(fao_cal, "fao_cal.csv", row.names=F)
+write.csv(fao_val, "fao_val.csv", row.names=F)
