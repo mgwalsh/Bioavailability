06de713ddfb8457addf7f5a6b0f809c685e035a5
mgwalsh <markusgwalsh@gmail.com>
2016-05-14 17:16:05 +0300

    documentation update

--- FAO_QUANT.R
+++ FAO_QUANT.R
@@ -18,7 +18,7 @@ suppressPackageStartupMessages({
 
 # Quantile regressions ----------------------------------------------------
 # Boron
-plot(pB~B, xlab="Boron content of soil (ppm)", ylab="Boron content of Wheat plants (ppm)", 
+plot(pB~B, xlab="Boron content of soil (ppm)", ylab="Boron content of wheat plants (ppm)", 
      xlim=c(-0.1,10.1), cex= 0.7, mirdat)
 BQ <- rq(log(pB)~log(B), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(BQ)
@@ -29,7 +29,7 @@ curve(exp(BQ$coefficients[7])*x^BQ$coefficients[8], add=T, from=0, to=9.5, col="
 curve(exp(BQ$coefficients[9])*x^BQ$coefficients[10], add=T, from=0, to=9.5, col="blue", lwd=1)
 
 # Copper
-plot(pCu~Cu, xlab="Copper content of soil (ppm)", ylab="Copper content of Wheat plants (ppm)", 
+plot(pCu~Cu, xlab="Copper content of soil (ppm)", ylab="Copper content of wheat plants (ppm)", 
      xlim=c(-0.1,60.1), cex= 0.7, mirdat)
 CuQ <- rq(log(pCu)~log(Cu), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(CuQ)
@@ -40,7 +40,7 @@ curve(exp(CuQ$coefficients[7])*x^CuQ$coefficients[8], add=T, from=0, to=58, col=
 curve(exp(CuQ$coefficients[9])*x^CuQ$coefficients[10], add=T, from=0, to=58, col="blue", lwd=1)
 
 # Manganese
-plot(pMn~Mn, xlab="Manganese content of soil (ppm)", ylab="Manganese content of Wheat plants (ppm)", 
+plot(pMn~Mn, xlab="Manganese content of soil (ppm)", ylab="Manganese content of wheat plants (ppm)", 
      xlim=c(-0.1,300.1), cex= 0.7, mirdat)
 MnQ <- rq(log(pMn)~log(Mn), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(MnQ)
@@ -51,7 +51,7 @@ curve(exp(MnQ$coefficients[7])*x^MnQ$coefficients[8], add=T, from=0, to=290, col
 curve(exp(MnQ$coefficients[9])*x^MnQ$coefficients[10], add=T, from=0, to=290, col="blue", lwd=1)
 
 # Molybdenum
-plot(pMo~Mo, xlab="Molybdenum content of soil (ppm)", ylab="Molybdenum content of Wheat plants (ppm)", 
+plot(pMo~Mo, xlab="Molybdenum content of soil (ppm)", ylab="Molybdenum content of wheat plants (ppm)", 
      xlim=c(-0.1,3.1), ylim=c(-0.1,4.1), cex= 0.7, mirdat)
 MoQ <- rq(log(pMo)~log(Mo), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(MoQ)
@@ -62,7 +62,7 @@ curve(exp(MoQ$coefficients[7])*x^MoQ$coefficients[8], add=T, from=0, to=2.9, col
 curve(exp(MoQ$coefficients[9])*x^MoQ$coefficients[10], add=T, from=0, to=2.9, col="blue", lwd=1)
 
 # Zinc
-plot(pZn~Zn, xlab="Zinc content of soil (ppm)", ylab="Zinc content of Wheat plants (ppm)", 
+plot(pZn~Zn, xlab="Zinc content of soil (ppm)", ylab="Zinc content of wheat plants (ppm)", 
      xlim=c(-0.1,200.1), cex= 0.7, mirdat)
 ZnQ <- rq(log(pZn)~log(Zn), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(ZnQ)
@@ -73,7 +73,7 @@ curve(exp(ZnQ$coefficients[7])*x^ZnQ$coefficients[8], add=T, from=0, to=190, col
 curve(exp(ZnQ$coefficients[9])*x^ZnQ$coefficients[10], add=T, from=0, to=190, col="blue", lwd=1)
 
 # Iron
-plot(pFe~Fe, xlab="Iron content of soil (ppm)", ylab="Iron content of Wheat plants (ppm)", 
+plot(pFe~Fe, xlab="Iron content of soil (ppm)", ylab="Iron content of wheat plants (ppm)", 
      xlim=c(-0.1,500.1), cex= 0.7, mirdat)
 FeQ <- rq(log(pFe)~log(Fe), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(FeQ)
