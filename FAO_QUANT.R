a4888cd4074a2081aa317f17934613797d27fee4
mgwalsh <markusgwalsh@gmail.com>
2016-12-15 20:44:27 +0300

    quantile regression update

--- FAO_QUANT.R
+++ FAO_QUANT.R
@@ -17,8 +17,10 @@ suppressPackageStartupMessages({
 # source_url(SourceURL)
 
 # Quantile regressions ----------------------------------------------------
+par(mfrow=c(1,2), mar=c(5,4.5,1,1))
+
 # Boron
-plot(pB~B, xlab="Boron content of soil (ppm)", ylab="Boron content of wheat plants (ppm)", 
+plot(pB~B, xlab="Boron content of soil (ppm)", ylab="Boron content of wheat plants (ppm)", cex.lab=1.2, 
      xlim=c(-0.1,10.1), cex= 0.7, mirdat)
 BQ <- rq(log(pB)~log(B), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(BQ)
@@ -62,7 +64,7 @@ curve(exp(MoQ$coefficients[7])*x^MoQ$coefficients[8], add=T, from=0, to=2.9, col
 curve(exp(MoQ$coefficients[9])*x^MoQ$coefficients[10], add=T, from=0, to=2.9, col="blue", lwd=1)
 
 # Zinc
-plot(pZn~Zn, xlab="Zinc content of soil (ppm)", ylab="Zinc content of wheat plants (ppm)", 
+plot(pZn~Zn, xlab="Zinc content of soil (ppm)", ylab="Zinc content of wheat plants (ppm)", cex.lab=1.2,
      xlim=c(-0.1,200.1), cex= 0.7, mirdat)
 ZnQ <- rq(log(pZn)~log(Zn), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(ZnQ)
