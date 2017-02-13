4227601d86c81aafcdd53b902dead261e8990aa8
mgwalsh <markusgwalsh@gmail.com>
2017-02-13 04:06:14 +0300

    Trace mineral soil-plant enrichment ratio models

--- FAO_er_models.R
+++ FAO_er_models.R
@@ -4,7 +4,7 @@
 #' M. Walsh, February 2017
 
 # Required packages
-# install.packages(c("devtools","quantreg")), dependencies=TRUE)
+# install.packages(c("devtools","arm")), dependencies=TRUE)
 suppressPackageStartupMessages({
   require(devtools)
   require(quantreg)
@@ -16,41 +16,50 @@ suppressPackageStartupMessages({
 # SourceURL <- "https://raw.githubusercontent.com/mgwalsh/Bioavailability/master/FAO_micro_setup.R"
 # source_url(SourceURL)
 
-# Basic allometry ---------------------------------------------------------
+# Allometric plots --------------------------------------------------------
 hidm <- mirdat[ which(mirdat$HL=='H'), ] ## select high dry matter production reference group
+par(mfrow=c(2,3), mar=c(5,4.5,1,1))
 
 # Boron
 bref <- quantile(hidm$pB/hidm$B, p=1/2)
 mirdat$dB <- (mirdat$pB/mirdat$B)/bref
-plot(log(dB)~log(B), mirdat)
-dB.lm <- lm(log(dB)~log(B), mirdat)
+plot(log(dB)~log(B), mirdat, xlab="log[Soil B]", cex=1.2, cex.lab=1.5)
+abline(h=0)
+abline(rq(log(dB)~log(B), tau=0.5, mirdat), col="red", lwd=2)
 
 # Copper
 curef <- quantile(hidm$pCu/hidm$Cu, p=1/2)
 mirdat$dCu <- (mirdat$pCu/mirdat$Cu)/curef
-plot(log(dCu)~log(Cu), mirdat)
-dCu.lm <- lm(log(dCu)~log(Cu), mirdat)
+plot(log(dCu)~log(Cu), mirdat, xlab="log[Soil Cu]", cex=1.2, cex.lab=1.5)
+abline(h=0)
+abline(rq(log(dCu)~log(Cu), tau=0.5, mirdat), col="red", lwd=2)
 
 # Manganese
 mnref <- quantile(hidm$pMn/hidm$Mn, p=1/2)
 mirdat$dMn <- (mirdat$pMn/mirdat$Mn)/mnref
-plot(log(dMn)~log(Mn), mirdat)
-dMn.lm <- lm(log(dMn)~log(Mn), mirdat)
+plot(log(dMn)~log(Mn), mirdat, xlab="log[Soil Mn]", cex=1.2, cex.lab=1.5)
+abline(h=0)
+abline(rq(log(dMn)~log(Mn), tau=0.5, mirdat), col="red", lwd=2)
 
 # Molybdenum
 moref <- quantile(hidm$pMo/hidm$Mo, p=1/2)
 mirdat$dMo <- (mirdat$pMo/mirdat$Mo)/moref
-plot(log(dMo)~log(Mo), mirdat)
-dMo.lm <- lm(log(dMo)~log(Mo), mirdat)
+plot(log(dMo)~log(Mo), mirdat, xlab="log[Soil Mo]", cex=1.2, cex.lab=1.5)
+abline(h=0)
+abline(rq(log(dMo)~log(Mo), tau=0.5, mirdat), col="red", lwd=2)
 
 # Zinc
 znref <- quantile(hidm$pZn/hidm$Zn, p=1/2)
 mirdat$dZn <- (mirdat$pZn/mirdat$Zn)/znref
-plot(log(dZn)~log(Zn), mirdat)
-dZn.lm <- lm(log(dZn)~log(Zn), mirdat)
+plot(log(dZn)~log(Zn), mirdat, xlab="log[Soil Zn]", cex=1.2, cex.lab=1.5)
+abline(h=0)
+abline(rq(log(dZn)~log(Zn), tau=0.5, mirdat), col="red", lwd=2)
 
 # Iron
 feref <- quantile(hidm$pFe/hidm$Fe, p=1/2)
 mirdat$dFe <- (mirdat$pFe/mirdat$Fe)/feref
-plot(log(dFe)~log(Fe), mirdat)
-dFe.lm <- lm(log(dFe)~log(Fe), mirdat)
+plot(log(dFe)~log(Fe), mirdat, xlab="log[Soil Fe]", cex=1.2, cex.lab=1.5)
+abline(h=0)
+abline(rq(log(dFe)~log(Fe), tau=0.5, mirdat), col="red", lwd=2)
+
+# dev.off()
