edfb760e0e19610a299c661136499cd90311258e
mgwalsh <markusgwalsh@gmail.com>
2017-02-15 06:58:10 +0300

    Wheat plant micro-nutrient bioavailability quantiles

--- FAO_quant_reg.R
+++ FAO_quant_reg.R
@@ -17,11 +17,11 @@ suppressPackageStartupMessages({
 # source_url(SourceURL)
 
 # Quantile regressions ----------------------------------------------------
-par(mfrow=c(2,3), mar=c(5,4.5,1,1))
+par(mfrow=c(2,3), mar=c(5,5,1,1))
 
 # Boron
-plot(pB~B, xlab="Soil B (ppm)", ylab="Plant B (ppm)", cex.lab=1.5, 
-     xlim=c(-0.1,10.1), cex=1, mirdat)
+plot(pB~B, xlab=expression(paste("B"[s], " (ppm)")), ylab=expression(paste("B"[p], " (ppm)")), cex.lab=1.5, 
+     xlim=c(-0.1,10.1), cex=1.2, mirdat)
 BQ <- rq(log(pB)~log(B), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(BQ)
 curve(exp(BQ$coefficients[1])*x^BQ$coefficients[2], add=T, from=0, to=9.5, col="blue", lwd=1)
@@ -31,8 +31,8 @@ curve(exp(BQ$coefficients[7])*x^BQ$coefficients[8], add=T, from=0, to=9.5, col="
 curve(exp(BQ$coefficients[9])*x^BQ$coefficients[10], add=T, from=0, to=9.5, col="blue", lwd=1)
 
 # Copper
-plot(pCu~Cu, xlab="Soil Cu (ppm)", ylab="Plant Cu (ppm)", cex.lab=1.5,
-     xlim=c(-0.1,60.1), cex=1, mirdat)
+plot(pCu~Cu, xlab=expression(paste("Cu"[s], " (ppm)")), ylab=expression(paste("Cu"[p], " (ppm)")), cex.lab=1.5,
+     xlim=c(-0.1,60.1), cex=1.2, mirdat)
 CuQ <- rq(log(pCu)~log(Cu), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(CuQ)
 curve(exp(CuQ$coefficients[1])*x^CuQ$coefficients[2], add=T, from=0, to=58, col="blue", lwd=1)
@@ -42,8 +42,8 @@ curve(exp(CuQ$coefficients[7])*x^CuQ$coefficients[8], add=T, from=0, to=58, col=
 curve(exp(CuQ$coefficients[9])*x^CuQ$coefficients[10], add=T, from=0, to=58, col="blue", lwd=1)
 
 # Manganese
-plot(pMn~Mn, xlab="Soil Mn (ppm)", ylab="Plant Mn (ppm)", cex.lab=1.5, 
-     xlim=c(-0.1,300.1), cex=1, mirdat)
+plot(pMn~Mn, xlab=expression(paste("Mn"[s], " (ppm)")), ylab=expression(paste("Mn"[p], " (ppm)")), cex.lab=1.5, 
+     xlim=c(-0.1,300.1), cex=1.2, mirdat)
 MnQ <- rq(log(pMn)~log(Mn), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(MnQ)
 curve(exp(MnQ$coefficients[1])*x^MnQ$coefficients[2], add=T, from=0, to=290, col="blue", lwd=1)
@@ -53,8 +53,8 @@ curve(exp(MnQ$coefficients[7])*x^MnQ$coefficients[8], add=T, from=0, to=290, col
 curve(exp(MnQ$coefficients[9])*x^MnQ$coefficients[10], add=T, from=0, to=290, col="blue", lwd=1)
 
 # Molybdenum
-plot(pMo~Mo, xlab="Soil Mo (ppm)", ylab="Plant Mo (ppm)", cex.lab=1.5,
-     xlim=c(-0.1,3.1), ylim=c(-0.1,4.1), cex=1, mirdat)
+plot(pMo~Mo, xlab=expression(paste("Mo"[s], " (ppm)")), ylab=expression(paste("Mo"[p], " (ppm)")), cex.lab=1.5,
+     xlim=c(-0.1,3.1), ylim=c(-0.1,4.1), cex=1.2, mirdat)
 MoQ <- rq(log(pMo)~log(Mo), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(MoQ)
 curve(exp(MoQ$coefficients[1])*x^MoQ$coefficients[2], add=T, from=0, to=2.9, col="blue", lwd=1)
@@ -64,8 +64,10 @@ curve(exp(MoQ$coefficients[7])*x^MoQ$coefficients[8], add=T, from=0, to=2.9, col
 curve(exp(MoQ$coefficients[9])*x^MoQ$coefficients[10], add=T, from=0, to=2.9, col="blue", lwd=1)
 
 # Zinc
-plot(pZn~Zn, xlab="Soil Zn (ppm)", ylab="Plant Zn (ppm)", cex.lab=1.5,
-     xlim=c(-0.1,200.1), cex=1, mirdat)
+# x11()
+# par(mfrow=c(1,1), mar=c(5,5,1,1))
+plot(pZn~Zn, xlab=expression(paste("Zn"[s], " (ppm)")), ylab=expression(paste("Zn"[p], " (ppm)")), cex.lab=1.5,
+     xlim=c(-0.1,200.1), cex=1.2, mirdat)
 ZnQ <- rq(log(pZn)~log(Zn), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(ZnQ)
 curve(exp(ZnQ$coefficients[1])*x^ZnQ$coefficients[2], add=T, from=0, to=190, col="blue", lwd=1)
@@ -73,10 +75,12 @@ curve(exp(ZnQ$coefficients[3])*x^ZnQ$coefficients[4], add=T, from=0, to=190, col
 curve(exp(ZnQ$coefficients[5])*x^ZnQ$coefficients[6], add=T, from=0, to=190, col="red", lwd=2)
 curve(exp(ZnQ$coefficients[7])*x^ZnQ$coefficients[8], add=T, from=0, to=190, col="blue", lty=2)
 curve(exp(ZnQ$coefficients[9])*x^ZnQ$coefficients[10], add=T, from=0, to=190, col="blue", lwd=1)
+# dev.copy(pdf, 'Zn_quant.pdf')
+# dev.off()
 
 # Iron
-plot(pFe~Fe, xlab="Soil Fe (ppm)", ylab="Plant Fe (ppm)", cex.lab=1.5,
-     xlim=c(-0.1,500.1), cex=1, mirdat)
+plot(pFe~Fe, xlab=expression(paste("Fe"[s], " (ppm)")), ylab=expression(paste("Fe"[p], " (ppm)")), cex.lab=1.5,
+     xlim=c(-0.1,500.1), cex=1.2, mirdat)
 FeQ <- rq(log(pFe)~log(Fe), tau=c(0.05,0.25,0.5,0.75,0.95), data=mirdat)
 print(FeQ)
 curve(exp(FeQ$coefficients[1])*x^FeQ$coefficients[2], add=T, from=0, to=490, col="blue", lwd=1)
