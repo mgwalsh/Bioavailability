46aa8540d077485f2face76800b37f634abde677
mgwalsh <markusgwalsh@gmail.com>
2017-03-10 10:13:26 +0300

    Graphical interaction models of greenhouse grown wheat plants from soil data

--- FAO_grim.R
+++ FAO_grim.R
@@ -16,14 +16,22 @@ suppressPackageStartupMessages({
 # source_url(SourceURL)
 
 # Select variables
-spx <- wetdat[c(32,4,7,9,33:42)]
+spx <- wetdat[c(32,4,9,7,33:42)]
 
 # Graphical models --------------------------------------------------------
 # Fit interaction model
 gm0 <- mmod(~.^., data = spx)
 gm1 <- stepwise(gm0)
 
-# Plot graph
+# Plot interaction graph
 dag <- ugList(terms(gm1), result="matrix")
 net <- as.network(x = dag, directed = FALSE, loops = FALSE, matrix.type = "adjacency")
 plot.network(net, vertex.col = "white", vertex.cex = 5, displaylabels = T, label.pos = 5, mode = "circle")
+
+# Fit naive Bayes model
+gm2 <- mmod(~pH:HL+CEC:HL+Sand:HL+V1:HL+V2:HL+V3:HL+V4:HL+V5:HL+V6:HL+V7:HL+V8:HL+V9:HL+V10:HL, data=spx)
+gm3 <- stepwise(gm2)
+dag <- ugList(terms(gm3), result="matrix")
+net <- as.network(x = dag, directed = FALSE, loops = FALSE, matrix.type = "adjacency")
+plot.network(net, vertex.col = "white", vertex.cex = 6, displaylabels = T, label.pos = 5, mode = "circle")
+
