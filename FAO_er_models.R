1319461ce2d6e486b23dc62e13d0410a65074147
mgwalsh <markusgwalsh@gmail.com>
2017-04-09 17:41:54 +0300

    Trace mineral soil-plant uptake/bioavailability models

--- FAO_er_models.R
+++ FAO_er_models.R
@@ -1,4 +1,4 @@
-#' Trace mineral soil-plant enrichment ratio models
+#' Trace mineral soil-plant uptake/bioavailability models
 #' Soil and wheat plant wet chemistry data courtesy of FAO (doc @ https://www.dropbox.com/s/gwk07tanhu86tqj/Silanpaa%20Report.pdf?dl=0)
 #' MIR soil data courtesy of ICRAF (2016)
 #' M. Walsh, February 2017
@@ -92,7 +92,7 @@ abline(v=log(sref[2]), lwd=2, col="red") ## median
 abline(v=log(sref[3]), col="blue") ## 95th precentile
 # abline(rq(log(dZn)~log(Zn), tau=0.05, hidm), col="blue", lwd=1)
 # abline(rq(log(dZn)~log(Zn), tau=0.5, hidm), col="red", lwd=2)
-#abline(rq(log(dZn)~log(Zn), tau=0.95, hidm), col="blue", lwd=1)
+# abline(rq(log(dZn)~log(Zn), tau=0.95, hidm), col="blue", lwd=1)
 # ZnQ <- rq(log(dZn)~log(Zn), tau=0.5, wetdat)
 # summary(ZnQ)
 # dev.copy(pdf, 'Zn_ER.pdf')
@@ -114,7 +114,36 @@ abline(v=log(sref[3]), col="blue") ## 95th precentile
 # dev.off() ## remember to turn graphics device off!
 
 # Quantreg models ---------------------------------------------------------
-# Zinc
-ZnQ <- rq(log(pZn/Zn)~HL*log(Zn), tau=c(0.05,0.5,0.95), data=wetdat)
-print(ZnQ)
-summary(ZnQ)
+# Zn uptake as f(DM yield)
+wetdat$uZn <- wetdat$pDM/10000*wetdat$pZn ## Zn uptake/pot
+ZnY <- rq(log(uZn)~log(pDM), tau=c(0.025,0.5,0.975), data=wetdat)
+print(ZnY)
+summary(ZnY)
+par(mfrow=c(1,3), mar=c(5,5,1,1))
+plot(uZn~pDM, wetdat, xlab="Dry matter (mg / pot)", xlim=c(0,2500), ylab="Zn uptake (mg / pot)", ylim=c(0,25), cex=1.1, cex.lab=1.3)
+curve(exp(ZnY$coefficients[1])*x^ZnY$coefficients[2], add=T, from=0, to=2500, col="blue", lwd=2)
+curve(exp(ZnY$coefficients[5])*x^ZnY$coefficients[6], add=T, from=0, to=2500, col="blue", lwd=2)
+curve(exp(ZnY$coefficients[3])*x^ZnY$coefficients[4], add=T, from=0, to=2500, col="red", lwd=2)
+
+# QUEFTS-type fit
+# ZnQ <- rq(log(pDM)~log(uZn), tau=c(0.025,0.5,0.975), data=wetdat)
+# print(ZnQ)
+# summary(ZnQ)
+# plot(pDM~uZn, wetdat, xlab="Zn uptake (mg / pot)", xlim=c(0,25), ylab="Dry matter (mg / pot)", ylim=c(0,2500), cex=1.1, cex.lab=1.3)
+# curve(exp(ZnQ$coefficients[1])*x^ZnQ$coefficients[2], add=T, from=0, to=25, col="blue", lwd=2)
+# curve(exp(ZnQ$coefficients[5])*x^ZnQ$coefficients[6], add=T, from=0, to=25, col="blue", lwd=2)
+# curve(exp(ZnQ$coefficients[3])*x^ZnQ$coefficients[4], add=T, from=0, to=25, col="red", lwd=2)
+
+# Zn uptake as f(Zn concentration in soil)
+ZnS <- rq(log(uZn)~log(Zn), tau=c(0.025,0.5,0.975), data=wetdat) ## Zn uptake/pot
+print(ZnS)
+summary(ZnS)
+plot(uZn~Zn, wetdat, xlab="Soil Zn (ppm)", xlim=c(0,200), ylim=c(0,25), ylab="Zn uptake (mg / pot)", cex=1.1, cex.lab=1.3)
+curve(exp(ZnS$coefficients[1])*x^ZnS$coefficients[2], add=T, from=0, to=200, col="blue", lwd=2)
+curve(exp(ZnS$coefficients[5])*x^ZnS$coefficients[6], add=T, from=0, to=200, col="blue", lwd=2)
+curve(exp(ZnS$coefficients[3])*x^ZnS$coefficients[4], add=T, from=0, to=200, col="red", lwd=2)
+
+# Zn uptake index as f(Zn concentration in soil & DM yield)
+ZnI <- rq(log(uZn)~log(Zn)*log(pDM), tau=c(0.025,0.5,0.975), data=wetdat) ## Zn uptake/pot
+print(ZnI)
+summary(ZnI)
